684451551bb3baf5e4a50ec82941cb7d9e66679b
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-11-08 18:01:15 +0100

    Added plotting of the p terms of the 1s orbitals in Helium to check they are zero.

--- share/test.py
+++ share/test.py
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 
 Z = 2
 dx = 1e-1/Z
-N = 160*Z
+N = 150*Z
 rmin = 1e-5
 h = hfnum.HF(dx, int(N), rmin, Z)
-h.addOrbital(0, 1, 0, 0)
-h.addOrbital(0, 1, 0, 0)
+h.addOrbital(1, 1, 0, 0)
+h.addOrbital(1, 1, 0, 0)
 NiterSCF = 1
 Niter = 100
 F0stop = 1e-16
@@ -29,10 +29,13 @@ for i in range(0, 5):
 
   r = np.asarray(h.getR())
   o1= np.asarray(h.getOrbital(0, 0, 0))
-  #o1l1m0  = h.getOrbital(0, 1, 0)
-  #o1l1mm1 = h.getOrbital(0, 1, -1)
-  #o1l1mp1 = h.getOrbital(0, 1,  1)
+  o1l1m0  = np.asarray(h.getOrbital(0, 1, 0))
+  o1l1mm1 = np.asarray(h.getOrbital(0, 1, -1))
+  o1l1mp1 = np.asarray(h.getOrbital(0, 1,  1))
   o2 = np.asarray(h.getOrbital(1, 0, 0))
+  o2l1m0  = np.asarray(h.getOrbital(1, 1, 0))
+  o2l1mm1 = np.asarray(h.getOrbital(1, 1, -1))
+  o2l1mp1 = np.asarray(h.getOrbital(1, 1,  1))
   v = h.getNucleusPotential()
   vd = h.getDirectPotential()
   H1s = 2*np.exp(-r)
@@ -41,10 +44,17 @@ for i in range(0, 5):
   f = plt.figure()
   plt.plot(r[:m], r[:m]*o1[:m], 'r-',  linewidth = 2, label = 'r*Orbital 0 (s)')
   plt.plot(r[:m], r[:m]*o2[:m], 'b:',  linewidth = 2, label = 'r*Orbital 1 (s)')
-  #plt.plot(r[:m], o1l1mm1[:m], 'g-.', linewidth = 2, label = 'Orbital 0 (p, m=-1)')
-  #plt.plot(r[:m], o1l1m0[:m],  'b--', linewidth = 2, label = 'Orbital 0 (p, m=0)')
-  #plt.plot(r[:m], o1l1mp1[:m], 'c:',  linewidth = 2, label = 'Orbital 0 (p, m=+1)')
   plt.plot(r[:m], r[:m]*H1s[:m], 'g--', linewidth = 2, label = 'r*Hydrogen 1s')
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  plt.plot(r[:m], r[:m]*o1l1mm1[:m], 'r-',  linewidth = 2, label = 'r*Orbital 0 (p1)')
+  plt.plot(r[:m], r[:m]*o1l1m0[:m], 'b-',  linewidth = 2, label = 'r*Orbital 0 (p2)')
+  plt.plot(r[:m], r[:m]*o1l1mp1[:m], 'g-',  linewidth = 2, label = 'r*Orbital 0 (p3)')
+  plt.plot(r[:m], r[:m]*o2l1mm1[:m], 'r:',  linewidth = 2, label = 'r*Orbital 1 (p1)')
+  plt.plot(r[:m], r[:m]*o2l1m0[:m], 'b:',  linewidth = 2, label = 'r*Orbital 1 (p2)')
+  plt.plot(r[:m], r[:m]*o2l1mp1[:m], 'g:',  linewidth = 2, label = 'r*Orbital 1 (p3)')
   plt.legend()
   plt.show()
 
