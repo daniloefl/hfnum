93b951a6685d7baa0f3765b864e1fce3e100b5f0
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-12-02 18:37:23 +0100

    Updated renormalised solver to use OrbitalMapper instead of calculating indices itself.

--- src/IterativeRenormalisedSolver.cxx
+++ src/IterativeRenormalisedSolver.cxx
@@ -56,7 +56,7 @@ ldouble IterativeRenormalisedSolver::solve(std::vector<ldouble> &E, std::vector<
   }
   match(matched, fix_inward, fix_outward);
 
-  return std::pow(F, 2);
+  return std::fabs(F);
 }
 
 void IterativeRenormalisedSolver::solveInward(std::vector<ldouble> &E, std::vector<int> &l, std::vector<MatrixXld> &Fm, std::vector<MatrixXld> &Km, std::vector<MatrixXld> &R) {
@@ -70,26 +70,15 @@ void IterativeRenormalisedSolver::solveInward(std::vector<ldouble> &E, std::vect
     R[i].resize(M, M);
   }
   R[N-1].setZero();
-  int idx = 0;
-  for (int k = 0; k < _o.size(); ++k) {
-    for (int l = 0; l < _o[k].L()+1; ++l) {
-      for (int m = -l; m < l+1; ++m) {
-        for (int i = N-1; i >= icl[0]-1; --i) {
-          R[i-1] = Km[i-1]*(MatrixXld::Identity(M, M)*12 - Fm[i-1]*10);
-          if (i < N-1 && R[i].determinant() != 0) R[i-1] -= R[i].inverse();
-        }
-        idx += 1;
-      }
-    }
+  for (int i = N-1; i >= icl[0]-1; --i) {
+    R[i-1] = Km[i-1]*(MatrixXld::Identity(M, M)*12 - Fm[i-1]*10);
+    if (i < N-1 && R[i].determinant() != 0) R[i-1] -= R[i].inverse();
   }
 }
 
 void IterativeRenormalisedSolver::solveOutward(std::vector<ldouble> &E, std::vector<int> &li, std::vector<MatrixXld> &Fm, std::vector<MatrixXld> &Km, std::vector<MatrixXld> &R) {
   int N = _g.N();
-  int M = 0;
-  for (int k = 0; k < _o.size(); ++k) {
-    M += 2*_o[k].L()+1;
-  }
+  int M = _om.N();
   R.resize(N);
   for (int i = 0; i < N; ++i) {
     R[i].resize(M, M);
@@ -99,42 +88,29 @@ void IterativeRenormalisedSolver::solveOutward(std::vector<ldouble> &E, std::vec
     R[0] = Fm[1]*Fm[0].inverse();
   }
 
-  int idx = 0;
-  for (int k = 0; k < _o.size(); ++k) {
-    for (int l = 0; l < _o[k].L()+1; ++l) {
-      for (int m = -l; m < l+1; ++m) {
-        for (int i = 1; i <= icl[0]+1; ++i) {
-          R[i] = Km[i]*(MatrixXld::Identity(M, M)*12 - Fm[i]*10);
-          if (R[i-1].determinant() != 0) R[i] -= R[i-1].inverse();
-        }
-        idx += 1;
-      }
-    }
+  for (int i = 1; i <= icl[0]+1; ++i) {
+    R[i] = Km[i]*(MatrixXld::Identity(M, M)*12 - Fm[i]*10);
+    if (R[i-1].determinant() != 0) R[i] -= R[i-1].inverse();
   }
 }
 void IterativeRenormalisedSolver::match(std::vector<VectorXld> &o, std::vector<VectorXld> &inward, std::vector<VectorXld> &outward) {
-  int M = 0;
-  for (int k = 0; k < _o.size(); ++k) {
-    M += 2*_o[k].L()+1;
-  }
+  int M = _om.N();
   for (int i = 0; i < _g.N(); ++i) {
     o[i].resize(M);
   }
 
-  int idx = 0;
-  for (int k = 0; k < _o.size(); ++k) {
-    ldouble ratio = outward[icl[0]](k)/inward[icl[0]](k);
-    for (int l = 0; l < _o[k].L()+1; ++l) {
-      for (int m = -l; m < l+1; ++m) {
-        for (int i = 0; i < _g.N(); ++i) {
-          if (i < icl[0]) {
-            o[i](idx) = outward[i](idx);
-          } else {
-            o[i](idx) = ratio*inward[i](idx);
-          }
-        }
-        idx += 1;
+  for (int idx = 0; idx < M; ++idx) {
+    int k = _om.orbital(idx);
+    int l = _om.l(idx);
+    int m = _om.m(idx);
+    ldouble ratio = outward[icl[0]](idx)/inward[icl[0]](idx);
+    for (int i = 0; i < _g.N(); ++i) {
+      if (i < icl[0]) {
+        o[i](idx) = outward[i](idx);
+      } else {
+        o[i](idx) = ratio*inward[i](idx);
       }
     }
   }
+
 }
