d5afc928f3e5eb84328469e6563fe1f192a67b25
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-12-31 01:40:13 +0100

    Added carbon test.

---
+++ share/test_C.py
@@ -0,0 +1,159 @@
+
+import sys
+sys.path.append("../lib/")
+sys.path.append("lib/")
+
+import numpy as np
+import hfnum
+
+import seaborn
+import matplotlib.pyplot as plt
+
+Z = 6
+
+# log grid
+dx = 1e-1/Z
+N = 220*Z
+rmin = 1e-8
+g = hfnum.Grid(True, dx, int(N), rmin)
+h = hfnum.HF(g, Z)
+h.method(2)
+
+orb0 = hfnum.Orbital( 1, 1, 0, 0)
+orb1 = hfnum.Orbital(-1, 1, 0, 0)
+orb2 = hfnum.Orbital( 1, 2, 0, 0)
+orb3 = hfnum.Orbital(-1, 2, 0, 0)
+orb4 = hfnum.Orbital( 1, 2, 1, 0)
+orb5 = hfnum.Orbital(-1, 2, 1, 0)
+h.addOrbital(orb0)
+h.addOrbital(orb1)
+h.addOrbital(orb2)
+h.addOrbital(orb3)
+h.addOrbital(orb4)
+h.addOrbital(orb5)
+
+NiterSCF = 1
+Niter = 1000
+F0stop = 1e-5
+r = np.asarray(g.getR())
+print "Last r:", r[-1]
+print "First r:", r[0:5]
+for i in range(0, 20):
+  print "SCF it.", i
+  h.gammaSCF(0.1)
+  h.solve(NiterSCF, Niter, F0stop)
+
+  o = [np.asarray(orb0.getCentral()), np.asarray(orb1.getCentral()), np.asarray(orb2.getCentral()), np.asarray(orb3.getCentral()), np.asarray(orb4.getCentral()), np.asarray(orb5.getCentral())]
+  v = h.getNucleusPotential()
+  vex = {}
+  vd = {}
+  vd[0] = h.getDirectPotential(0)
+  vex[0] = [h.getExchangePotential(0, 0), h.getExchangePotential(0, 1), h.getExchangePotential(0, 2), h.getExchangePotential(0, 3), h.getExchangePotential(0, 4), h.getExchangePotential(0, 5)]
+  vd[1] = h.getDirectPotential(1)
+  vex[1] = [h.getExchangePotential(1, 0), h.getExchangePotential(1, 1), h.getExchangePotential(1, 2), h.getExchangePotential(1, 3), h.getExchangePotential(1, 4), h.getExchangePotential(1, 5)]
+  vd[2] = h.getDirectPotential(2)
+  vex[2] = [h.getExchangePotential(2, 0), h.getExchangePotential(2, 1), h.getExchangePotential(2, 2), h.getExchangePotential(2, 3), h.getExchangePotential(2, 4), h.getExchangePotential(2, 5)]
+  vd[3] = h.getDirectPotential(3)
+  vex[3] = [h.getExchangePotential(3, 0), h.getExchangePotential(3, 1), h.getExchangePotential(3, 2), h.getExchangePotential(3, 3), h.getExchangePotential(3, 4), h.getExchangePotential(3, 5)]
+  vd[4] = h.getDirectPotential(4)
+  vex[4] = [h.getExchangePotential(4, 0), h.getExchangePotential(4, 1), h.getExchangePotential(4, 2), h.getExchangePotential(4, 3), h.getExchangePotential(4, 4), h.getExchangePotential(4, 5)]
+  vd[5] = h.getDirectPotential(5)
+  vex[5] = [h.getExchangePotential(5, 0), h.getExchangePotential(5, 1), h.getExchangePotential(5, 2), h.getExchangePotential(5, 3), h.getExchangePotential(5, 4), h.getExchangePotential(5, 5)]
+  H1s = 2*np.exp(-r)
+
+  m = next(i for i,v in enumerate(r) if v >= 10)
+  f = plt.figure()
+  plt.plot(r[:m], r[:m]*o[0][:m], 'r--',  linewidth = 2, label = 'r*Orbital 0 (s)')
+  plt.plot(r[:m], r[:m]*o[1][:m], 'b--',  linewidth = 2, label = 'r*Orbital 1 (s)')
+  plt.plot(r[:m], r[:m]*o[2][:m], 'm--',  linewidth = 2, label = 'r*Orbital 2 (s)')
+  plt.plot(r[:m], r[:m]*o[3][:m], 'c--',  linewidth = 2, label = 'r*Orbital 3 (s)')
+  plt.plot(r[:m], r[:m]*o[4][:m], 'k--',  linewidth = 2, label = 'r*Orbital 4 (p)')
+  plt.plot(r[:m], r[:m]*o[5][:m], 'g--',  linewidth = 2, label = 'r*Orbital 5 (p)')
+  #plt.plot(r[:m], r[:m]*H1s[:m], 'g--', linewidth = 2, label = 'r*Hydrogen 1s')
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  ymin = np.fabs(vd[0][0])
+  plt.plot(r[:m], v[:m], 'r-', linewidth = 3, label = 'Coulomb')
+  plt.plot(r[:m], vd[0][:m], 'b-', linewidth = 3, label = 'Direct (0)')
+  plt.plot(r[:m], vex[0][0][:m], 'g--', linewidth = 3, label = 'Exchange (0,0)')
+  plt.plot(r[:m], vex[0][1][:m], 'm--', linewidth = 3, label = 'Exchange (0,1)')
+  plt.plot(r[:m], vex[0][2][:m], 'c--', linewidth = 3, label = 'Exchange (0,2)')
+  plt.plot(r[:m], vex[0][3][:m], 'k--', linewidth = 3, label = 'Exchange (0,3)')
+  plt.plot(r[:m], vex[0][4][:m], 'y--', linewidth = 3, label = 'Exchange (0,4)')
+  plt.plot(r[:m], vex[0][5][:m], 'r--', linewidth = 3, label = 'Exchange (0,5)')
+  plt.ylim((-ymin, ymin))
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  ymin = np.fabs(vd[1][0])
+  plt.plot(r[:m], v[:m], 'r-', linewidth = 3, label = 'Coulomb')
+  plt.plot(r[:m], vd[1][:m], 'b-', linewidth = 3, label = 'Direct (1)')
+  plt.plot(r[:m], vex[1][0][:m], 'g--', linewidth = 3, label = 'Exchange (1,0)')
+  plt.plot(r[:m], vex[1][1][:m], 'm--', linewidth = 3, label = 'Exchange (1,1)')
+  plt.plot(r[:m], vex[1][2][:m], 'c--', linewidth = 3, label = 'Exchange (1,2)')
+  plt.plot(r[:m], vex[1][3][:m], 'k--', linewidth = 3, label = 'Exchange (1,3)')
+  plt.plot(r[:m], vex[1][4][:m], 'y--', linewidth = 3, label = 'Exchange (1,4)')
+  plt.plot(r[:m], vex[1][5][:m], 'r--', linewidth = 3, label = 'Exchange (1,5)')
+  plt.ylim((-ymin, ymin))
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  ymin = np.fabs(vd[2][0])
+  plt.plot(r[:m], v[:m], 'r-', linewidth = 3, label = 'Coulomb')
+  plt.plot(r[:m], vd[2][:m], 'b-', linewidth = 3, label = 'Direct (2)')
+  plt.plot(r[:m], vex[2][0][:m], 'g--', linewidth = 3, label = 'Exchange (2,0)')
+  plt.plot(r[:m], vex[2][1][:m], 'm--', linewidth = 3, label = 'Exchange (2,1)')
+  plt.plot(r[:m], vex[2][2][:m], 'c--', linewidth = 3, label = 'Exchange (2,2)')
+  plt.plot(r[:m], vex[2][3][:m], 'k--', linewidth = 3, label = 'Exchange (2,3)')
+  plt.plot(r[:m], vex[2][4][:m], 'y--', linewidth = 3, label = 'Exchange (2,4)')
+  plt.plot(r[:m], vex[2][5][:m], 'r--', linewidth = 3, label = 'Exchange (2,5)')
+  plt.ylim((-ymin, ymin))
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  ymin = np.fabs(vd[3][0])
+  plt.plot(r[:m], v[:m], 'r-', linewidth = 3, label = 'Coulomb')
+  plt.plot(r[:m], vd[3][:m], 'b-', linewidth = 3, label = 'Direct (3)')
+  plt.plot(r[:m], vex[3][0][:m], 'g--', linewidth = 3, label = 'Exchange (3,0)')
+  plt.plot(r[:m], vex[3][1][:m], 'm--', linewidth = 3, label = 'Exchange (3,1)')
+  plt.plot(r[:m], vex[3][2][:m], 'c--', linewidth = 3, label = 'Exchange (3,2)')
+  plt.plot(r[:m], vex[3][3][:m], 'k--', linewidth = 3, label = 'Exchange (3,3)')
+  plt.plot(r[:m], vex[3][4][:m], 'y--', linewidth = 3, label = 'Exchange (3,4)')
+  plt.plot(r[:m], vex[3][5][:m], 'r--', linewidth = 3, label = 'Exchange (3,5)')
+  plt.ylim((-ymin, ymin))
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  ymin = np.fabs(vd[4][0])
+  plt.plot(r[:m], v[:m], 'r-', linewidth = 3, label = 'Coulomb')
+  plt.plot(r[:m], vd[4][:m], 'b-', linewidth = 3, label = 'Direct (4)')
+  plt.plot(r[:m], vex[4][0][:m], 'g--', linewidth = 3, label = 'Exchange (4,0)')
+  plt.plot(r[:m], vex[4][1][:m], 'm--', linewidth = 3, label = 'Exchange (4,1)')
+  plt.plot(r[:m], vex[4][2][:m], 'c--', linewidth = 3, label = 'Exchange (4,2)')
+  plt.plot(r[:m], vex[4][3][:m], 'k--', linewidth = 3, label = 'Exchange (4,3)')
+  plt.plot(r[:m], vex[4][4][:m], 'y--', linewidth = 3, label = 'Exchange (4,4)')
+  plt.plot(r[:m], vex[4][5][:m], 'r--', linewidth = 3, label = 'Exchange (4,5)')
+  plt.ylim((-ymin, ymin))
+  plt.legend()
+  plt.show()
+
+  f = plt.figure()
+  ymin = np.fabs(vd[5][0])
+  plt.plot(r[:m], v[:m], 'r-', linewidth = 3, label = 'Coulomb')
+  plt.plot(r[:m], vd[5][:m], 'b-', linewidth = 3, label = 'Direct (5)')
+  plt.plot(r[:m], vex[5][0][:m], 'g--', linewidth = 3, label = 'Exchange (5,0)')
+  plt.plot(r[:m], vex[5][1][:m], 'm--', linewidth = 3, label = 'Exchange (5,1)')
+  plt.plot(r[:m], vex[5][2][:m], 'c--', linewidth = 3, label = 'Exchange (5,2)')
+  plt.plot(r[:m], vex[5][3][:m], 'k--', linewidth = 3, label = 'Exchange (5,3)')
+  plt.plot(r[:m], vex[5][4][:m], 'y--', linewidth = 3, label = 'Exchange (5,4)')
+  plt.plot(r[:m], vex[5][5][:m], 'r--', linewidth = 3, label = 'Exchange (5,5)')
+  plt.ylim((-ymin, ymin))
+  plt.legend()
+  plt.show()
+
