203ffbe84338b312e471885fda0776d661ff0e36
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-01-03 23:17:35 +0100

    First attempt on perturbation correction.

--- src/HF.cxx
+++ src/HF.cxx
@@ -38,6 +38,7 @@ HF::HF(const std::string fname)
 HF::~HF() {
 }
 
+// TODO fix this ... gives non-sensical results now
 std::vector<ldouble> HF::perturbativeLSEnergy() {
   // define delta V = (full Vd - full Vex) - (Vd - Vex)
   std::vector<ldouble> E1(_o.size(), 0);
@@ -51,7 +52,7 @@ std::vector<ldouble> HF::perturbativeLSEnergy() {
         ldouble r = (*_g)(ir);
         ldouble dr = 0;
         if (ir < _g->N()-1) dr = (*_g)(ir+1) - (*_g)(ir);
-        E1[k] += -_vd[k2][ir]*std::pow(_o[k]->getNorm(ir, tlm.l, tlm.m, *_g), 2)*std::pow(r, 2)*dr;
+        E1[k] += -0.5*_vd[k2][ir]*std::pow(_o[k2]->getNorm(ir, tlm.l, tlm.m, *_g), 2)*std::pow(r, 2)*dr;
       }
     }
 
@@ -67,11 +68,44 @@ std::vector<ldouble> HF::perturbativeLSEnergy() {
     //}
 
     // TODO calculate correction based on full spherical harmonics
-    std::map<lm, Vradial> full;
-    full[tlm] = Vradial(_g->N(), 0);
-
     // + full vd
-    // vd = sum_k2 int |psi_k2(r) Y_k2(Omega)|^2 dOmega r^2 dr
+    // vd(r1) = sum_k2 int |psi_k2(r2) Y_k2(Omega2)|^2 1/|r1 - r2| dOmega2 r2^2 dr2
+    // E1 term will be: Term = sum_k2 [int dr1 dOmega1 r1^2 psi_k(r1)^2 Y*_k(Omega1)^2 { int dr2 dOmega2 r2^2 psi_k2(r2)^2 Y_k2(Omega2)^2 1/|r1 - r2| } ]
+    // Term = sum_k2 int dr1 int dOmega1 int dr2 int dOmega2 [ r1^2 psi_k(r1)^2 Y*_k(Omega1)^2 r2^2 psi_k2(r2)^2 Y_k2(Omega2)^2 1/|r1 - r2| ]
+    // 1/|r1 - r2| = sum_l=0^inf sum_m=-l^+l 4 pi/(2*l+1) r_<^l/r_>^(l+1) Y_lm*(Omega1) Y_lm(Omega2)
+    // Term = sum_k2 int dr1 int dOmega1 int dr2 int dOmega2 sum_l=0^inf sum_m=-l^+l 4 pi/(2*l+1) [ r1^2 r2^2 r_<^l/r_>^(l+1) psi_k(r1)^2 psi_k2(r2)^2 Y*_k(O1)^2 Y_k2(O2)^2 Y_lm(O1) Y_lm*(O2) ]
+    // int dO1 Y*_k(O1)^2 Y_lm(O1) = int dO1 Y_(l_k, -m_k)(O1)^2 Y_lm(O1) = (-1)^m (2l_k+1)/sqrt(4pi(2l+1)) CG(l_k, l_k, 0, 0, l, 0) CG(l_k, l_k, -m_k, -m_k, l, -m)
+    // int dO2 Y_k2(O2)^2 Y_lm*(O2) = (-1)^m int dO2 Y_(l_k2, m_k2)(O1)^2 Y_(l,-m)(O1) = (2l_k2+1)/sqrt(4pi(2l+1)) CG(l_k2, l_k2, 0, 0, l, 0) CG(l_k2, l_k2, m_k2, m_k2, l, m)
+    // Term = sum_k2 int dr1 int dr2 sum_l=0^inf sum_m=-l^+l [ (2*l_k+1) * (2*l_k2+1) / (2*l+1)^2 (-1)^m CG(l_k, l_k, 0, 0, l, 0) * CG(l_k2, l_k2, 0, 0, l, 0) * CG(l_k, l_k, -m_k, -m_k, l, -m) * CG(l_k2, l_k2, m_k2, m_k2, l, m) ] * [ r1^2 r2^2 r_<^l/r_>^(l+1) psi_k(r1)^2 psi_k2(r2)^2 ]
+    //
+    int lmax = 2; // approximated here
+    for (int k2 = 0; k2 < _o.size(); ++k2) {
+      lm tlm2(_o[k2]->initialL(), _o[k2]->initialM());
+      for (int ir1 = 0; ir1 < _g->N(); ++ir1) {
+        ldouble r1 = (*_g)(ir1);
+        ldouble dr1 = 0;
+        if (ir1 < _g->N()-1) dr1 = (*_g)(ir1+1) - (*_g)(ir1);
+
+        for (int ir2 = 0; ir2 < _g->N(); ++ir2) {
+          ldouble r2 = (*_g)(ir2);
+          ldouble dr2 = 0;
+          if (ir2 < _g->N()-1) dr2 = (*_g)(ir2+1) - (*_g)(ir2);
+
+          ldouble rsmall = r1;
+          ldouble rlarge = r2;
+          if (r2 < r1) {
+            rsmall = r2;
+            rlarge = r1;
+          }
+
+          for (int l = 0; l < lmax; ++l) {
+            for (int m = -l; m <= l; ++m) {
+              E1[k] += std::pow(-1, m)*(2.0*tlm2.l+1.0)*(2.0*tlm.l+1.0)/(2.0*l+1.0)*CG(tlm.l, tlm.l, 0, 0, l, 0)*CG(tlm2.l, tlm2.l, 0, 0, l, 0)*CG(tlm.l, tlm.l, -tlm.m, -tlm.m, l, -m)*CG(tlm2.l, tlm2.l, tlm2.m, tlm2.m, l, m)*std::pow(_o[k]->getNorm(ir1, tlm.l, tlm.m, *_g)*_o[k2]->getNorm(ir2, tlm2.l, tlm2.m, *_g), 2)*std::pow(r1*r2, 2)*std::pow(rsmall, l)/std::pow(rlarge, l+1)*dr1*dr2;
+            }
+          }
+        }
+      }
+    }
   }
   return E1;
 }
