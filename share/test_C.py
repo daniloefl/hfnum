cc6c6c28c7680efb0324ac890546bea7b330d9d3
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-01-05 00:10:15 +0100

    Using two spin-orbitals with spin up in p orbitals for C.

--- share/test_C.py
+++ share/test_C.py
@@ -13,7 +13,7 @@ Z = 6
 
 # log grid
 dx = 0.5*1e-1/Z
-N = 2*225*Z
+N = 2*210*Z
 rmin = 1e-8
 h = hfnum.HF()
 h.resetGrid(True, dx, int(N), rmin)
@@ -25,7 +25,7 @@ orb1 = hfnum.Orbital(-1, 1, 0, 0)
 orb2 = hfnum.Orbital( 1, 2, 0, 0)
 orb3 = hfnum.Orbital(-1, 2, 0, 0)
 orb4 = hfnum.Orbital( 1, 2, 1, 0)
-orb5 = hfnum.Orbital(-1, 2, 1, 0)
+orb5 = hfnum.Orbital( 1, 2, 1,-1)
 h.addOrbital(orb0)
 h.addOrbital(orb1)
 h.addOrbital(orb2)
@@ -35,7 +35,7 @@ h.addOrbital(orb5)
 
 NiterSCF = 40
 Niter = 1000
-F0stop = 1e-6
+F0stop = 1e-3
 r = np.asarray(h.getR())
 print "Last r:", r[-1]
 print "First r:", r[0:5]
