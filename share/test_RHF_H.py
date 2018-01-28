a26e4d5063764707b6bbe27eade6b59eecf514dd
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-01-28 13:43:06 +0100

    Added Helium test for rHF eqs.

--- share/test_RHF_H.py
+++ share/test_RHF_H.py
@@ -9,24 +9,35 @@ import hfnum
 import seaborn
 import matplotlib.pyplot as plt
 
-Z = 1
+Z = 2
 
 h = hfnum.RHF()
 h.setZ(Z)
 h.addOrbital(1, 0, 0,  1)
+h.addOrbital(1, 0, 0, -1)
 h.solve()
 
 r = []
-for i in range(0, 150):
+for i in range(0, 130):
   r.append(np.exp(np.log(1e-4) + i*1e-1))
 
 o_up = []
-o_up.append(h.getOrbital(0, 1, 0, 0, r))
+for i in range(0, h.getNOrbitals(1)):
+  l = h.getOrbital_l(i, 1)
+  m = h.getOrbital_m(i, 1)
+  o_up.append(h.getOrbital(i, 1, l, m, r))
 
-m = next(i for i,v in enumerate(r) if v >= 10)
+o_dw = []
+for i in range(0, h.getNOrbitals(-1)):
+  l = h.getOrbital_l(i, -1)
+  m = h.getOrbital_m(i, -1)
+  o_dw.append(h.getOrbital(i, -1, l, m, r))
+
+m = next(i for i,v in enumerate(r) if v >= 5)
 f = plt.figure()
 plt.plot(r[:m], 2*np.exp(-np.asarray(r[:m])), 'b:',  linewidth = 2, label = 'H 1s')
-plt.plot(r[:m], o_up[0][:m], 'r--',  linewidth = 2, label = 'Orbital 0 (s)')
+plt.plot(r[:m], o_up[0][:m], 'r--',  linewidth = 2, label = 'Orbital 0 up (s)')
+plt.plot(r[:m], o_dw[0][:m], 'g--',  linewidth = 2, label = 'Orbital 0 dw (s)')
 plt.legend()
 plt.show()
 
