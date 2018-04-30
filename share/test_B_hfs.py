a86034787495224ec691d927f597c24acce1e19b
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-04-30 14:31:43 +0200

    Added updated B tests.

--- share/test_B_hfs.py
+++ share/test_B_hfs.py
@@ -10,21 +10,21 @@ import seaborn
 import matplotlib.pyplot as plt
 
 Z = 5
-dx = 0.5*1e-1/Z
-N = 2*225*Z
-rmin = 1e-8
+
+# log grid
 dx = 0.5e-1
-N = 441
+N = 421
 rmin = 1e-8
-h = hfnum.DFT()
+h = hfnum.HFS()
 h.resetGrid(True, dx, int(N), rmin)
 h.setZ(Z)
+h.method(2)
+
 orb0 = hfnum.Orbital( 1, 1, 0, 0)
 orb1 = hfnum.Orbital(-1, 1, 0, 0)
 orb2 = hfnum.Orbital( 1, 2, 0, 0)
 orb3 = hfnum.Orbital(-1, 2, 0, 0)
 orb4 = hfnum.Orbital( 1, 2, 1, 0)
-h.method(2)
 h.addOrbital(orb0)
 h.addOrbital(orb1)
 h.addOrbital(orb2)
@@ -32,13 +32,12 @@ h.addOrbital(orb3)
 h.addOrbital(orb4)
 
 NiterSCF = 40
-Niter = 100
-F0stop = 1e-5
-r = h.getR()
-r = np.asarray(r)
+Niter = 1000
+F0stop = 1e-6
+r = np.asarray(h.getR())
 print "Last r:", r[-1]
 print "First r:", r[0:5]
-h.gammaSCF(0.4)
+h.gammaSCF(0.3)
 h.solve(NiterSCF, Niter, F0stop)
-h.save('output/results_B_dft.txt')
+h.save("output/results_B_hfs.txt")
 
