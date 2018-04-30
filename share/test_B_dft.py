8da27196b9b4c50a6bc1540d051734b61a838180
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-04-30 16:48:36 +0200

    Added Boron dft test.

--- share/test_B_dft.py
+++ share/test_B_dft.py
@@ -10,21 +10,18 @@ import seaborn
 import matplotlib.pyplot as plt
 
 Z = 5
-
-# log grid
 dx = 0.5e-1
 N = 421
 rmin = 1e-8
-h = hfnum.HFS()
+h = hfnum.DFT()
 h.resetGrid(True, dx, int(N), rmin)
 h.setZ(Z)
-h.method(2)
-
 orb0 = hfnum.Orbital( 1, 1, 0, 0)
 orb1 = hfnum.Orbital(-1, 1, 0, 0)
 orb2 = hfnum.Orbital( 1, 2, 0, 0)
 orb3 = hfnum.Orbital(-1, 2, 0, 0)
-orb4 = hfnum.Orbital( 1, 2, 1, 0)
+orb4 = hfnum.Orbital( 1, 2, 1, 1)
+h.method(2)
 h.addOrbital(orb0)
 h.addOrbital(orb1)
 h.addOrbital(orb2)
@@ -33,11 +30,12 @@ h.addOrbital(orb4)
 
 NiterSCF = 40
 Niter = 100
-F0stop = 1e-6
-r = np.asarray(h.getR())
+F0stop = 1e-5
+r = h.getR()
+r = np.asarray(r)
 print "Last r:", r[-1]
 print "First r:", r[0:5]
-h.gammaSCF(0.2)
+h.gammaSCF(0.3)
 h.solve(NiterSCF, Niter, F0stop)
-h.save("output/results_B_hfs.txt")
+h.save('output/results_B_dft.txt')
 
