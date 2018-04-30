7c3082d0a22f3962a691671f21da7bbe1f970516
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-04-30 14:33:15 +0200

    Updated grid.

--- share/test_B+.py
+++ share/test_B+.py
@@ -12,18 +12,8 @@ import matplotlib.pyplot as plt
 Z = 5
 
 # log grid
-dx = 0.5*1e-1/Z
-N = 2*310*Z
-rmin = 1e-12
 dx = 0.5e-1
-N = 210*2
-rmin = 1e-8
-dx = 0.4e-1
-N = 260*2
-rmin = 1e-8
-
-dx = 0.5e-1
-N = 441
+N = 421
 rmin = 1e-8
 h = hfnum.HF()
 h.resetGrid(True, dx, int(N), rmin)
