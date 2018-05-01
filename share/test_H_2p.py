d5259040b5a4f781a30a2b341f85af7ff87b1557
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-05-01 10:48:45 +0200

    Checked that the standard method works with the H 2p orbital.

--- share/test_H_2p.py
+++ share/test_H_2p.py
@@ -12,16 +12,13 @@ import matplotlib.pyplot as plt
 Z = 1
 
 # log grid
-dx = 1e-2
-N = 1900
-rmin = 1e-7
 dx = 0.5e-1
-N = 441
+N = 421
 rmin = 1e-8
 h = hfnum.HF()
 h.resetGrid(True, dx, int(N), rmin)
 h.setZ(Z)
-h.method(2)
+h.method(3)
 
 orb = hfnum.Orbital(1, 2, 1, 0)
 
@@ -38,11 +35,15 @@ h.save('output/results_H_2p.txt')
 o = [np.asarray(orb.getCentral())]
 v = h.getNucleusPotential()
 H1s = 2*np.exp(-r)
+H2s = 1.0/(2.0*np.sqrt(2))*(2 - r)*np.exp(-r/2.0)
+H2p = 1.0/(2.0*np.sqrt(6))*r*np.exp(-r/2.0)
 
-m = next(i for i,v in enumerate(r) if v >= 10)
+m = -1 #next(i for i,v in enumerate(r) if v >= 10)
 f = plt.figure()
 plt.plot(r[:m], r[:m]*o[0][:m], 'r-',  linewidth = 2, label = 'r*Orbital 0 (s)')
 plt.plot(r[:m], r[:m]*H1s[:m], 'g--', linewidth = 2, label = 'r*Hydrogen 1s')
+plt.plot(r[:m], r[:m]*H2s[:m], 'b--', linewidth = 2, label = 'r*Hydrogen 2s')
+plt.plot(r[:m], r[:m]*H2p[:m], 'm--', linewidth = 2, label = 'r*Hydrogen 2p')
 plt.legend()
 plt.show()
 
