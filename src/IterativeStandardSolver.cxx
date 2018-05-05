aa7361c8dc6aacd481729a5ef7e73aba281780b2
Danilo Ferreira de Lima <daniloefl@gmail.com>
2018-05-05 12:49:22 +0200

    Updated standard solver to invert radial functions at infinity if the number of nodes is expected to be odd.

--- src/IterativeStandardSolver.cxx
+++ src/IterativeStandardSolver.cxx
@@ -197,6 +197,10 @@ void IterativeStandardSolver::solveInward(std::vector<ldouble> &E, std::map<int,
   solution.resize(N);
   solution[N-1] = std::pow(_g(N-1), _Z/std::sqrt(2*std::fabs(E[idx]))-0.5)*std::exp(-std::sqrt(2*std::fabs(E[idx]))*_g(N-1));
   solution[N-2] = std::pow(_g(N-2), _Z/std::sqrt(2*std::fabs(E[idx]))-0.5)*std::exp(-std::sqrt(2*std::fabs(E[idx]))*_g(N-2));
+  if ((_o[idx]->n() - _o[idx]->l() - 1) % 2 == 1) {
+    solution[N-1] *= -1;
+    solution[N-2] *= -1;
+  }
   for (int k = N-2; k >= 1; --k) {
     solution[k-1] = ((12 - f[idx][k]*10)*solution[k] - f[idx][k+1]*solution[k+1] - s[idx][k-1] - s[idx][k] - s[idx][k+1])/f[idx][k-1];
   }
@@ -209,10 +213,10 @@ void IterativeStandardSolver::solveOutward(std::vector<ldouble> &E, std::map<int
   solution[1] = std::exp(-_g(1)/_o[idx]->n())*std::pow(_g(1), _o[idx]->l() + 0.5);
   //solution[0] = std::pow(_g(0), _o[idx]->l() + 0.5);
   //solution[1] = std::pow(_g(1), _o[idx]->l() + 0.5);
-  if ((_o[idx]->n() - _o[idx]->l() - 1) % 2 == 1) {
-    solution[0] *= -1;
-    solution[1] *= -1;
-  }
+  //if ((_o[idx]->n() - _o[idx]->l() - 1) % 2 == 1) {
+  //  solution[0] *= -1;
+  //  solution[1] *= -1;
+  //}
 
   for (int k = 1; k < N-2; ++k) {
     solution[k+1] = ((12.0 - f[idx][k]*10.0)*solution[k] - f[idx][k-1]*solution[k-1] - s[idx][k-1] - s[idx][k] - s[idx][k+1])/f[idx][k+1];
