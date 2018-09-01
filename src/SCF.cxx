59e46ac50c2325921562f13dc1cee57770424f10
Danilo Enoque Ferreira De Lima <danilo.enoque.ferreira.de.lima@cern.ch>
2018-09-01 14:47:46 +0200

    Larger Grid to avoid numerical error.

--- src/SCF.cxx
+++ src/SCF.cxx
@@ -32,7 +32,7 @@ using namespace boost;
 #include "HFException.h"
 
 SCF::SCF(ldouble Z)
-  : _g(new Grid(expGrid, 1.0/32.0, (int) ((std::log(16.0) + 6 + std::log(Z))/(1.0/32.0)), std::exp(-6)/Z)),
+  : _g(new Grid(expGrid, 1.0/8.0, (int) ((std::log(16.0) + 4 + std::log(Z))/(1.0/8.0)), std::exp(-4)/Z)),
   _Z(Z), _om(*_g, _o), _lsb(*_g, _o, icl, _om), _iss(*_g, _o, icl, _om) {
   _own_grid = true;
   _pot.resize(_g->N());
